db1c1f65e339030cc053f56884afc38b6ceb44d2
imirza7 <127257130+imirza7@users.noreply.github.com>
2023-10-07 19:17:30 -0400

    Update contact.js

---
+++ app/routes/contact.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+// const app = express();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+router.get('/project', function(req, res, next) {
+  res.render('project', { title: 'Express' });
+});
+router.get('/service', function(req, res, next) {
+  res.render('service', { title: 'Express' });
+});
+router.get("/partials/header", function (req, res, next) {
+  res.render("header", { title: "Header" });
+});
